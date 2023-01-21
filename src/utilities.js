850408d04d091cea8544f666deb94ab1b48eb371
toddbrentlinger <toddbrentlinger@gmail.com>
2023-01-20 18:53:55 -0800

    Start utility function to add commas to number

--- src/utilities.js
+++ src/utilities.js
@@ -16,3 +16,44 @@
 
     return element;
 }
+
+export function addCommasToNumber(num) {
+    // If num is NOT a number, try first converting to num
+    // Ex. String '1.2' can be converted to a number
+    if (typeof num !== 'number') {
+        num = Number(num);
+    }
+
+    // If num is still NOT a number, return unchanged
+    if (typeof num !== 'number') {
+        return num;
+    }
+
+    // If reach this point, num is confirmed to be of type 'number'
+
+    // Convert num to string
+    num = num.toString();
+
+    // Find first index to start, either at the end or a decimal
+    let i = num.search(/\.|e/); // Index of '.', 'e', -1 if not present
+
+    // If '.' or 'e' NOT present, set to end of num string
+    if (i < 0) {
+        i = num.length;
+    }
+
+    let counter = 0;
+    while (i > 0) {
+        if (counter < 3) {
+            counter++;
+        } else {
+            // Add comma (cannot be first index from while loop condition)
+            num = num.slice(0, i) + ',' + num.slice(i);
+
+            counter = 0;
+        }
+        i--;
+    }
+
+    return num;
+}
