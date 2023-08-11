020906d23e3e4fcf52e3c886a5f67a16bdbf7571
toddbrentlinger <toddbrentlinger@gmail.com>
2023-08-11 16:04:57 -0700

    Install and setup gh-pages and font awesome packages
    Installs gh-pages npm package and sets up deploy scripts.
    Install font awesome npm package and import icons into index.js file.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './styles/meyerReset.scss';
 import './styles/styles.scss';
 import weatherApp from './weatherApp.js';
+import '@fortawesome/fontawesome-free/js/fontawesome.min.js';
+import '@fortawesome/fontawesome-free/js/solid.min.js';
 
 weatherApp.render(document.body);
 weatherApp.init();
